949b6562afd9a1927844aadcdbbaf7a61223997c
krut_ <egorrak3221@mail.ru>
2023-02-01 20:01:23 +0300

    Начал делать Дз

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Task1();
+        Task2();
+        Task3();
+
+    }
+
+
+    public static void Task1(){
+        System.out.println("Задание1");
+    }
+    public static void Task2() {
+        System.out.println("Задание2");
+    }
+    public static void Task3() {
+        System.out.println("Задание3");
+    }
+}
