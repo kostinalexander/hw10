a4e39c14c071cc92cc4b1b8032ceed80a7619c61
krut_ <egorrak3221@mail.ru>
2023-02-05 18:32:07 +0300

    Переделал

--- src/Main.java
+++ src/Main.java
@@ -1,69 +1,46 @@
+import java.time.LocalDate;
+
 public class Main {
-    public static void printYearLeap(int yearLeap) {
-        if (yearLeap % 4 == 0 && yearLeap % 100 != 0 || yearLeap % 400 == 0) {
-            System.out.println("год високосный");
-        } else  {
-            System.out.println("год не високосный");
-        }
-    }
-    public static void printOsSystem(int year,int os) {
-        if(year>=2015 && os==0){
-            System.out.println("Установите версию на сайте для IOS");
-        } else if (year<2015 && os==0){
-            System.out.println("Установите облегчённую версию для IOS");
-        }if (year>=2015 && os==1){
-            System.out.println("Усатновите версию на сайте для Андроид");
-        } else if (year<2015 && os==1) {
-            System.out.println("Установите облегчённую версию для Андроид");
-        }
-    }
-    public static void printDeliveryDays(int distant){
-        int time = 0;
-
-        if (distant<=20){
-            time = 1;
-            System.out.println("Доставка займёт "+ time+ " день");
-        }
-        if (distant>=20 && distant<60) {
-            time = time+1;
-            System.out.println("Доставка займёт "+ time+ " дня");
-        }
-        if (distant>=60 && distant<100){
-            time = time+2;
-            System.out.println("Доставка займёт "+ time+ " дня");
-        }
-        if (distant>100){
-            System.out.println("Доставки нет");
+    public static void main(String[] args) {
+        var yearLeap = 2020;
+        if (isYearLeap(yearLeap)){
+            System.out.println(yearLeap + " Год високосный");
+        } else {
+            System.out.println( yearLeap + " Год не високосный");
         }
 
-    }
-
-
-    public static void main (String[]args){
-        Task1();
-        Task2();
-        Task3();
-    }
-
-
-    public static void Task1 () {
-        System.out.println("Задание1");
-        int year = 2021;
-        printYearLeap(year);
-    }
-
-    public static void Task2 () {
-        System.out.println("Задание2");
-        int os = 0;
-        int year =2010;
-        printOsSystem(year,os );
-    }
-    public static void Task3 () {
-        System.out.println("Задание3");
-        int deliveryDistant = 11;
-        printDeliveryDays(deliveryDistant );
+     printAppType(1, 2011);
+        int days = countDeliveryDays(6);
+        if (days >0) {
+            System.out.println("Доставка займёт "+ days+ " день");
+        }
 
 
     }
+    public static int countDeliveryDays(int deliveryDistance) {
+        if (deliveryDistance>100){
+            return -1;
+        }
+        int days = 1;
+        if (deliveryDistance>20){
+            days++;
+        }
+        if (deliveryDistance>60){
+            days++;
+        }
+        return days;
+    }
+    public static void printAppType(int os, int year){
+        int currentYear = LocalDate.now().getYear();
+        String osName = os == 0 ? " iOs " : " Anroid";
+        String appType = year == currentYear ? " обычную " : " облегчённую ";
+        System.out.println(" установите "+ appType+ " версию приложения для "+ osName);
 
-}
+    }
+    public static boolean isYearLeap(int yearLeap) {
+        return  yearLeap % 4 == 0 && yearLeap % 100 != 0 || yearLeap % 400 == 0;
+        }
+
+        }
+
+
